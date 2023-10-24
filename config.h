19d0d2737f7140565b947e92cce7c9ad5d4f5150
Yelyzaveta Berezhna <yberezhn@student.42london.com>
2023-10-24 15:28:09 +0100

    bsq: added files2

--- config.h
+++ config.h
@@ -1,8 +1,28 @@
-//
-// Created by Yelyzaveta Berezhna on 24/10/2023.
-//
+#include "ft_def.h"
 
-#ifndef SCHOOL42_CONFIG_H
-#define SCHOOL42_CONFIG_H
+#ifndef CONFIG_H
+#define CONFIG_H
 
-#endif //SCHOOL42_CONFIG_H
+static const t_sq_element g_sq_ary[16] = {
+
+				{{0, 0}, true},
+				{{1, 0}},
+				{{2, 0}},
+				{{3, 0}},
+				{{0, 1}},
+				{{1, 1}},
+				{{2, 1}},
+				{{3, 1}},
+				{{0, 2}, true},
+				{{1, 2}},
+				{{2, 2}},
+				{{3, 2}},
+				{{0, 3}},
+				{{1, 3}},
+				{{2, 3}},
+				{{3, 3}}
+};
+
+static const int g_sq_ary_len = 16;
+
+#endif //CONFIG_H
--- sq_utils.c
+++ sq_utils.c
@@ -1,3 +1,35 @@
-//
-// Created by Yelyzaveta Berezhna on 24/10/2023.
-//
+#include <stdbool.h>
+#include "ft_def.h"
+
+
+t_sq_element create_element(int x, int y, bool is_obstacle)
+{
+	t_sq_element temp;
+
+	temp.pos.x = x;
+	temp.pos.y = y;
+	temp.is_obstacle = is_obstacle;
+	return (temp);
+};
+
+t_sq_pos create_pos(int x, int y)
+{
+	t_sq_pos temp;
+
+	temp.x = x;
+	temp.y = y;
+	return (temp);
+};
+
+int get_el_size_by_pos(int x, int y)
+{
+	if (x < 0 || y < 0)
+		return (0);
+};
+
+void set_size(t_sq_element el, int size)
+{
+	//TODO
+	el.size = size;
+
+}
