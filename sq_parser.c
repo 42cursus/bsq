0243fd54edb779d41c5d589a934bf3d1b8737ac2
Yelyzaveta Berezhna <yberezhn@student.42london.com>
2023-10-25 13:38:50 +0100

    added sq_parser

---
+++ sq_parser.c
@@ -0,0 +1,32 @@
+//#include "bsq_def.h"
+
+//bool	is_legend_valid(t_map_legend l)
+//{
+//	if (l.line_count )
+//}
+//
+//t_map_legend	get_legend(char *buf)
+//{
+//	t_map_legend	l;
+//
+//	l.line_count = buf[0] - '0';
+//	l.empty = buf[1];
+//	l.obstacle = buf[2];
+//	l.bsq = buf[3];
+//	is_legend_valid(l);
+//
+//	return (l);
+//}
+//
+//t_baguette	parse(char *buf)
+//{
+//
+//	t_baguette		b;
+//
+//	l.line_count = buf[0] - '0';
+//	l.empty = buf[1];
+//	l.obstacle = buf[2];
+//	l.bsq = buf[3];
+//
+//};
+
